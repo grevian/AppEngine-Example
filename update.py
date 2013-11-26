09b380ecc15fd291efdeb6a64c1131f173dfc14a
Joshua Hayes-Sheen <grevian+github@gmail.com>
2013-11-26 01:50:50 -0500

    Correct syntax around article queries, improve rating algorithm

--- update.py
+++ update.py
@@ -13,26 +13,29 @@ def update_ratings(article_id=None):
     # Get all articles except those who have a rating so low no one would see them
     # Do a keys_only query because it's faster and because the keys are all we need here
     query = Article.query(default_options=ndb.QueryOptions(keys_only=True)).filter(Article.rating >= -10)
-    for article in query:
-      deferred.defer(update_ratings, article_id=article.key.id())
+    for article_key in query:
+      deferred.defer(update_ratings, article_id=article_key.id())
     return
 
   # Update only the given article
   article = Article.get_by_id(article_id)
 
   # Get a sum of both upvotes and downvotes at the same time
-  upvote_count_future = Upvote.query(article=article.key).count_async()
-  downvote_count_future = Downvote.query(article=article.key).count_async()
+  upvote_count_future = Upvote.query().filter(Upvote.article==article.key).count_async()
+  downvote_count_future = Downvote.query().filter(Downvote.article==article.key).count_async()
 
   # Calculate the ratios of upvotes to downvotes
-  rating = float(upvote_count_future.get_result()) / float(downvote_count_future.get_result())
+  upvotes = max(upvote_count_future.get_result(), 1)
+  downvotes = max(downvote_count_future.get_result(), 1)
+  rating = float(upvotes) / float(downvotes)
 
   # Articles decay over time, by 0.5 per hour so figure out the decay and subtract it from the article rating
-  decay = (article.submitted - datetime.now()).seconds/60
+  decay = (article.submitted - datetime.now()).seconds/60/60
   rating = rating - (decay * 0.5)
 
   # Update the article and write it out
   logging.info("Updating Article %d to rating %f" % (article_id, rating))
+  logging.info("Decay was %f, there were %d upvotes and %d downvotes" % (decay, upvotes, downvotes))
   article.rating = rating
   article.put()
 
