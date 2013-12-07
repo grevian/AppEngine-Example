627e8391e42ce7f72354a8e33e213e984c6f570f
Joshua Hayes-Sheen <grevian+github@gmail.com>
2013-12-06 22:48:19 -0500

    Refactor comment system, simplify and restrict votes to one per user per article

--- models/content.py
+++ models/content.py
@@ -10,17 +10,6 @@ class Article(ndb.Model):
   submitter = ndb.KeyProperty(kind=JedditUser)
   rating = ndb.FloatProperty(default=0.5)
 
-# Two models used to calculate the content rating
-class Upvote(ndb.Model):
-  article = ndb.KeyProperty(kind=Article)
-  user = ndb.KeyProperty(kind=JedditUser)
-  voted = ndb.DateTimeProperty(auto_now_add=True)
-
-class Downvote(ndb.Model):
-  article = ndb.KeyProperty(kind=Article)
-  user = ndb.KeyProperty(kind=JedditUser)
-  voted = ndb.DateTimeProperty(auto_now_add=True)
-
 class Comment(ndb.Model):
   article = ndb.KeyProperty(kind=Article)
   user = ndb.KeyProperty(kind=JedditUser)
--- vote.py
+++ vote.py
@@ -1,27 +1,28 @@
 import webapp2
-import os
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
-from models.content import Article, Upvote, Downvote
+from models.content import Article
+from models.vote import Vote
+from models.vote import UPVOTE, DOWNVOTE
 from models.auth import JedditUser
 
 class AddVoteHandler(webapp2.RequestHandler):
 
   def post(self, article_id, vote_type): 
     article = Article.get_by_id(int(article_id))
+
     user = users.get_current_user()
+    if user:
+      user_key = JedditUser.key_from_user(user)
     
     if vote_type == 'down':
-      vote = Upvote(article=article.key)
+      vote = Vote.create(article_key=article.key, user_key=user_key, value=DOWNVOTE)
       article.rating = article.rating - 1.0
     else:
-      vote = Downvote(article=article.key)
+      vote = Vote.create(article_key=article.key, user_key=user_key, value=UPVOTE)
       article.rating = article.rating + 1.0
-    
-    if user:
-      vote.user = JedditUser.key_from_user(user)
 
     ndb.put_multi([article, vote])
 
