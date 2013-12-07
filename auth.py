1a6fc14d577544aa8268ed4a4d06805bd14bf8bd
Joshua Hayes-Sheen <grevian+github@gmail.com>
2013-12-06 22:45:53 -0500

    simplify imports, add logging

--- auth.py
+++ auth.py
@@ -1,17 +1,9 @@
-import jinja2
 import logging
-import os
 import webapp2
 
-from google.appengine.api import memcache
 from google.appengine.api import users
 
-from models.content import Article, Comment
 from models.auth import JedditUser
-
-# This just says to load templates from the same directory this file exists in
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname('resources/templates/')))
 
 class LoginHandler(webapp2.RequestHandler):
 
@@ -29,10 +21,12 @@ class LoginHandler(webapp2.RequestHandler):
       # Use a user entity in our datastore so we can get their nickname etc. for other users to see
       # Also to tie content created by them on Jeddit back to their user account
       existing_user = JedditUser.get_or_create_by_user(user)
+      logging.info("%s has logged in" % existing_user.user.email())
       
       # The docs indicate that an existing user could change their email address or nickname, which could require
       # the user entity to be updated, so handle that case here
       if existing_user.user != user:
+        logging.info("User %s has updated their google account")
         existing_user.user = user
         new_user.put()
 
@@ -42,6 +36,7 @@ class LoginHandler(webapp2.RequestHandler):
       return self.redirect('/', body="Thanks for logging in")
     else:
       # You could display a login form here if you had alternative methods of logging in
+      logging.warning("A user could not be logged in")
       self.response.write('You could not be logged in')
 
 def user_vars():
